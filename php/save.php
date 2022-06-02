c98c91c4d322a00bc67e6259896f8a75eac66eca
anikavaish01 <91093534+anikavaish01@users.noreply.github.com>
2022-06-01 18:05:51 -0700

    Delete php directory

--- php/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $file_path = $_POST['directory_path'] . '/' . $_POST['file_name'];
-    $file_content = $_POST['data'];
-
-    $handle = fopen($file_path, 'a');
-    fwrite($handle, $file_content);
-    fclose($handle);
-    chmod($file_path, 0777);
-?>
